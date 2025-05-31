4c071be6f196ce1cc2ee47355dd7071db0fd29ba
Jayendra <voutlajay8765@gmail.com>
2025-05-31 15:56:50 +0530

    Add plugin to copy index.html to 404.html during build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,24 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import fs from "fs";
+
+// Plugin to copy index.html to 404.html
+function copy404Plugin() {
+  return {
+    name: "copy-index-to-404",
+    closeBundle: () => {
+      fs.copyFileSync("dist/index.html", "dist/404.html");
+    },
+  };
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Task-Management/',
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    copy404Plugin()
   ],
 });
